f7453b094e4d2740e45fc7ba3477e29f75ac8da8
Sever Banesiu <banesiu.sever@gmail.com>
2019-10-21 18:29:01 -0700

    Optimize Val's type check

--- new/val.h
+++ new/val.h
@@ -18,15 +18,17 @@
 typedef struct {
   union {
     double d;
-    uint64_t u;
+    uint64_t u; // for bit fiddling
   } as;
 } Val;
 
 // Vals are 64 bits long and use a bit-packing technique called NaN tagging.
 static_assert(sizeof(uint64_t) == sizeof(double), "double size mismatch");
 
-static inline uint64_t val_u(Val v) { return v.as.u; }
-static inline Val u_val(uint64_t v) { return (Val){.as = {.u = v}}; }
+#define VAL_U(v) ((v).as.u)
+#define VAL_D(v) ((v).as.d)
+#define U_VAL(v) ((Val){.as = {.u = (v)}})
+#define D_VAL(v) ((Val){.as = {.d = (v)}})
 
 // BYTES is a visual aid for defining 64 bit patterns.
 #define BYTES(a, b, c, d, e, f, g, h) (UINT64_C(0x##a##b##c##d##e##f##g##h))
@@ -53,16 +55,16 @@ static inline Val u_val(uint64_t v) { return (Val){.as = {.u = v}}; }
 // remaining six bytes.
 
 #define TAGGED_MASK BYTES(7f, f4, 00, 00, 00, 00, 00, 00)
-#define IS_TAGGED(v) ((val_u(v) & TAGGED_MASK) == TAGGED_MASK)
+#define IS_TAGGED(v) ((VAL_U(v) & TAGGED_MASK) == TAGGED_MASK)
 #define IS_DOUBLE(v) (!IS_TAGGED(v))
 
 // We define two categories of values: pointers and data. Pointers have the
 // most significant discriminator bit (the sign bit) set, while data values
 // have it unset.
 #define SIGN_FLAG BYTES(80, 00, 00, 00, 00, 00, 00, 00)
-#define IS_PTR(v) ((val_u(v) & (TAGGED_MASK | SIGN_FLAG)) == TAGGED_MASK)
+#define IS_PTR(v) ((VAL_U(v) & (TAGGED_MASK | SIGN_FLAG)) == TAGGED_MASK)
 #define IS_DATA(v)                                                             \
-  ((val_u(v) && (TAGGED_MASK | SIGN_FLAG)) == (TAGGED_MASK | SIGN_FLAG))
+  ((VAL_U(v) && (TAGGED_MASK | SIGN_FLAG)) == (TAGGED_MASK | SIGN_FLAG))
 
 // Pointers have additional tagging applied to their least significant bit.
 // This is possible because pointers allocated with malloc are usually aligned
@@ -75,14 +77,14 @@ static inline Val u_val(uint64_t v) { return (Val){.as = {.u = v}}; }
 static_assert(sizeof(max_align_t) >= 2, "pointer alginment >= 2");
 #define OWNER_FLAG BYTES(00, 00, 00, 00, 00, 00, 00, 01)
 #define IS_OWN_PTR(v)                                                          \
-  ((val_u(v) & (SIGN_FLAG | TAGGED_MASK | OWNER_FLAG)) == TAGGED_MASK)
+  ((VAL_U(v) & (SIGN_FLAG | TAGGED_MASK | OWNER_FLAG)) == TAGGED_MASK)
 #define IS_REF_PTR(v)                                                          \
-  ((val_u(v) & (SIGN_FLAG | TAGGED_MASK | OWNER_FLAG)) ==                      \
+  ((VAL_U(v) & (SIGN_FLAG | TAGGED_MASK | OWNER_FLAG)) ==                      \
    (TAGGED_MASK | OWNER_FLAG))
 
 // PTR_MASK isolates the pointer value and ignores the ownership flag.
 #define PTR_MASK BYTES(00, 00, FF, FF, FF, FF, FF, FE)
-#define PTR(v) ((void *)(uintptr_t)(val_u(v) & PTR_MASK))
+#define PTR(v) ((void *)(uintptr_t)(VAL_U(v) & PTR_MASK))
 
 // TYPE_MASK isolates the type discriminator from the stored value.
 #define TYPE_MASK BYTES(ff, ff, 00, 00, 00, 00, 00, 00)
@@ -91,39 +93,39 @@ static_assert(sizeof(max_align_t) >= 2, "pointer alginment >= 2");
 // following layout:
 // 01111111|11110100|........|........|........|........|........|.......o
 #define STR_PTR_TYPE BYTES(7f, f4, 00, 00, 00, 00, 00, 00)
-#define IS_STR_PTR(v) ((val_u(v) & TYPE_MASK) == STR_PTR_TYPE)
-#define IS_STR_OWN(v) ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == STR_PTR_TYPE)
+#define IS_STR_PTR(v) ((VAL_U(v) & TYPE_MASK) == STR_PTR_TYPE)
+#define IS_STR_OWN(v) ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == STR_PTR_TYPE)
 #define IS_STR_REF(v)                                                          \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == (STR_PTR_TYPE | OWNER_FLAG))
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == (STR_PTR_TYPE | OWNER_FLAG))
 #define STR_PTR(v) ((String *)PTR(v))
 
 // Next, we define the Table pointer which has the following layout:
 // 01111111|11110101|........|........|........|........|........|.......o
 #define TABLE_PTR_TYPE BYTES(7f, f5, 00, 00, 00, 00, 00, 00)
-#define IS_TABLE_PTR(v) (val_u(v) & TYPE_MASK == TABLE_PTR_TYPE)
+#define IS_TABLE_PTR(v) (VAL_U(v) & TYPE_MASK == TABLE_PTR_TYPE)
 #define IS_TABLE_OWN(v)                                                        \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == TABLE_PTR_TYPE)
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == TABLE_PTR_TYPE)
 #define IS_TABLE_REF(v)                                                        \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == (TABLE_PTR_TYPE | OWNER_FLAG))
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == (TABLE_PTR_TYPE | OWNER_FLAG))
 #define TABLE_PTR(v) ((Table *)PTR(v))
 
 // The Array pointer is the next pointer value type:
 // 01111111|11110110|........|........|........|........|........|.......o
 #define ARRAY_PTR_TYPE BYTES(7f, f6, 00, 00, 00, 00, 00, 00)
-#define IS_ARRAY_PTR(v) (val_u(v) & TYPE_MASK == ARRAY_PTR_TYPE)
+#define IS_ARRAY_PTR(v) (VAL_U(v) & TYPE_MASK == ARRAY_PTR_TYPE)
 #define IS_ARRAY_OWN(v)                                                        \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == ARRAY_PTR_TYPE)
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == ARRAY_PTR_TYPE)
 #define IS_ARRAY_REF(v)                                                        \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == (ARRAY_PTR_TYPE | OWNER_FLAG))
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == (ARRAY_PTR_TYPE | OWNER_FLAG))
 #define ARRAY_PTR(v) ((Array *)PTR(v))
 
 // Lastly, we have a "big" 64-bit signed integer pointer (i.e., int64_t):
 // 01111111|11110111|........|........|........|........|........|.......o
 #define INT_PTR_TYPE BYTES(7f, f7, 00, 00, 00, 00, 00, 00)
-#define IS_INT_PTR(v) ((val_u(v) & TYPE_MASK) == INT_PTR_TYPE)
-#define IS_INT_OWN(v) ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == INT_PTR_TYPE)
+#define IS_INT_PTR(v) ((VAL_U(v) & TYPE_MASK) == INT_PTR_TYPE)
+#define IS_INT_OWN(v) ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == INT_PTR_TYPE)
 #define IS_INT_REF(v)                                                          \
-  ((val_u(v) & (TYPE_MASK | OWNER_FLAG)) == (INT_PTR_TYPE | OWNER_FLAG))
+  ((VAL_U(v) & (TYPE_MASK | OWNER_FLAG)) == (INT_PTR_TYPE | OWNER_FLAG))
 #define INT_PTR(v) ((int64_t *)PTR(v))
 
 // The remaining four pointer value type are reserved for later use:
@@ -144,9 +146,9 @@ static_assert(sizeof(max_align_t) >= 2, "pointer alginment >= 2");
 #define PAIR_DATA_TYPE BYTES(ff, f4, 00, 00, 00, 00, 00, 00)
 #define PAIR_A_MASK BYTES(00, 00, ff, ff, 00, 00, 00, 00)
 #define PAIR_B_MASK BYTES(00, 00, 00, 00, ff, ff, ff, ff)
-#define IS_PAIR_DATA(v) ((val_u(v) & TYPE_MASK) == PAIR_DATA_TYPE)
-#define PAIR_UA(v) ((uint16_t)((val_u(v) & PAIR_A_MASK) >> 32))
-#define PAIR_UB(v) ((uint32_t)(val_u(v) & PAIR_B_MASK))
+#define IS_PAIR_DATA(v) ((VAL_U(v) & TYPE_MASK) == PAIR_DATA_TYPE)
+#define PAIR_UA(v) ((uint16_t)((VAL_U(v) & PAIR_A_MASK) >> 32))
+#define PAIR_UB(v) ((uint32_t)(VAL_U(v) & PAIR_B_MASK))
 
 int16_t u16_i(uint16_t v) {
   union {
@@ -173,28 +175,28 @@ uint32_t u32_i(int32_t v) {
 // for user-defined symbols and flags.
 // 11111111|11110101|uuuuuuuu|uuuuuuuu|uuuuuuuu|uuuuuuuu|........|........
 #define SYMB_DATA_TYPE BYTES(ff, f5, 00, 00, 00, 00, 00, 00)
-#define IS_SYMBOL_DATA(v) ((val_u(v) & TYPE_MASK) == SYMB_DATA_TYPE)
-#define VAL_FALSE (u_val(SYMB_DATA_TYPE))
-#define VAL_TRUE (u_val(SYMB_DATA_TYPE + 1))
-#define VAL_NIL (u_val(SYMB_DATA_TYPE + 2))
-#define VAL_ERROR (u_val(SYMB_DATA_TYPE + 3))
-#define IS_FALSE(v) (val_u(v) == VAL_FALSE)
-#define IS_TRUE(v) (val_u(v) == VAL_TRUE)
-#define IS_NIL(v) (val_u(v) == VAL_NIL)
-#define IS_ERROR(v) (val_u(v) == VAL_ERROR)
+#define IS_SYMBOL_DATA(v) ((VAL_U(v) & TYPE_MASK) == SYMB_DATA_TYPE)
+#define VAL_FALSE (U_VAL(SYMB_DATA_TYPE))
+#define VAL_TRUE (U_VAL(SYMB_DATA_TYPE + 1))
+#define VAL_NIL (U_VAL(SYMB_DATA_TYPE + 2))
+#define VAL_ERROR (U_VAL(SYMB_DATA_TYPE + 3))
+#define IS_FALSE(v) (VAL_U(v) == VAL_FALSE)
+#define IS_TRUE(v) (VAL_U(v) == VAL_TRUE)
+#define IS_NIL(v) (VAL_U(v) == VAL_NIL)
+#define IS_ERROR(v) (VAL_U(v) == VAL_ERROR)
 
 static inline bool is_res_symbol(Val v) {
-  return (val_u(v) >= SYMB_DATA_TYPE &&
-          val_u(v) <= SYMB_DATA_TYPE + BYTES(00, 00, 00, 00, 00, 00, ff, ff));
+  return (VAL_U(v) >= SYMB_DATA_TYPE &&
+          VAL_U(v) <= SYMB_DATA_TYPE + BYTES(00, 00, 00, 00, 00, 00, ff, ff));
 }
 
 static inline bool is_usr_symbol(Val v) {
-  return (val_u(v) >= SYMB_DATA_TYPE + BYTES(00, 00, 00, 00, 00, 01, 00, 00) &&
-          val_u(v) <= SYMB_DATA_TYPE + BYTES(00, 00, ff, ff, ff, ff, ff, ff));
+  return (VAL_U(v) >= SYMB_DATA_TYPE + BYTES(00, 00, 00, 00, 00, 01, 00, 00) &&
+          VAL_U(v) <= SYMB_DATA_TYPE + BYTES(00, 00, ff, ff, ff, ff, ff, ff));
 }
 
 #define USR_SYMBOL_MASK BYTES(00, 00, ff, ff, ff, ff, 00, 00)
-#define USR_SYMBOL(v) ((uint32_t)((val_u(v) & USR_SYMBOL_MASK) >> 16))
+#define USR_SYMBOL(v) ((uint32_t)((VAL_U(v) & USR_SYMBOL_MASK) >> 16))
 
 // The remaining six data value types are reserved for later use:
 // 11111111|11110110|........|........|........|........|........|........
@@ -204,22 +206,20 @@ static inline bool is_usr_symbol(Val v) {
 // 11111111|11111110|........|........|........|........|........|........
 // 11111111|11111111|........|........|........|........|........|........
 
-// ValType makes writing switch statements on a value's type less error prone:
+// ValType makes switching on a Val's type less error prone and faster:
+#define VAL_ENUM(v) ((int)(uint8_t)(((v) >> 48) ^ ((v) >> 56)))
 typedef enum {
-  STR_PTR_T = (int)(STR_PTR_TYPE >> 48), // 0x7ff4
+  STR_PTR_T = VAL_ENUM(STR_PTR_TYPE), // 0x8B
   TABLE_PTR_T,
   ARRAY_PTR_T,
-  INT_PTR_T,
-  // NB: these are negative values for 16-bit ints, but the order is still OK.
-  DOUBLE_DATA_T = (int)(PAIR_DATA_TYPE >> 48) - 1, // 0xfff4 - 1
-  PAIR_DATA_T,
+  INT_PTR_T, // NB: next PTR type must skip some values
+  DOUBLE_DATA_T = VAL_ENUM(PAIR_DATA_TYPE) - 1, // 0x0a
+  PAIR_DATA_T,                                  // 0x0b
   SYMB_DATA_T,
 } ValType;
 
 static inline ValType type(Val v) {
-  if (IS_DOUBLE(v))
-    return DOUBLE_DATA_T;
-  return (ValType)((val_u(v) & TYPE_MASK) >> 48);
+  return IS_DOUBLE(v) ? DOUBLE_DATA_T : VAL_ENUM(VAL_U(v));
 }
 
 #endif
