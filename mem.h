c3ec6bfa68632424f22f7e87cc943ddd20cb693d
Sever Banesiu <banesiu.sever@gmail.com>
2019-10-23 15:42:20 -0700

    Add mem guards for overflow.

--- mem.h
+++ mem.h
@@ -3,23 +3,34 @@
 
 #include <stddef.h>
 
-#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
-
-#define GROW_ARRAY(pointer, type, old_count, new_count)                        \
-  reallocate(pointer, sizeof(type) * (old_count), sizeof(type) * (new_count))
-
-#define FREE_ARRAY(pointer, type, old_count)                                   \
-  reallocate(pointer, sizeof(type) * (old_count), 0)
-
-#define ALLOCATE(type) (type *)reallocate(NULL, 0, sizeof(type))
-
+#define ALLOCATE(type) (type *)reallocate(0, 0, sizeof(type))
 #define FREE(pointer, type) reallocate(pointer, sizeof(type), 0)
 
-#define ALLOCATE_FLEX(type, array_type, length)                                \
-  reallocate(NULL, 0, sizeof(type) + sizeof(array_type) * length)
+#define GROW_ARRAY(pointer, type, old_count, new_count)                        \
+  ((((old_count) < SIZE_MAX / sizeof(type)) &&                                 \
+    ((new_count) < SIZE_MAX / sizeof(type)))                                   \
+       ? reallocate((pointer), sizeof(type) * (old_count),                     \
+                    sizeof(type) * (new_count))                                \
+       : 0)
+#define FREE_ARRAY(pointer, type, old_count)                                   \
+  do {                                                                         \
+    if ((old_count) < SIZE_MAX / sizeof(type))                                 \
+      reallocate((pointer), sizeof(type) * (old_count), 0);                    \
+    else                                                                       \
+      assert(0);                                                               \
+  } while (0);
 
-#define FREE_FLEX(pointer, type, array_type, length)                           \
-  reallocate(pointer, sizeof(type) + sizeof(array_type) * length, 0)
+#define ALLOCATE_FLEX(type, array_type, len)                                   \
+  ((len) < ((SIZE_MAX - sizeof(type)) / sizeof(array_type))                    \
+       ? reallocate(0, 0, sizeof(type) + sizeof(array_type) * (len))           \
+       : 0)
+#define FREE_FLEX(pointer, type, array_type, len)                              \
+  do {                                                                         \
+    if ((len) < (SIZE_MAX - sizeof(type)) / sizeof(array_type))                \
+      reallocate((pointer), sizeof(type) + sizeof(array_type) * (len), 0);     \
+    else                                                                       \
+      assert(0);                                                               \
+  } while (0);
 
 void *reallocate(void *previous, size_t oldSize, size_t newSize);
 
