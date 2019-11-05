3e61530545f4fbb6b3fe4729cdc7af94ffd8d165
Sever Banesiu <banesiu.sever@gmail.com>
2019-11-05 01:00:31 -0800

    bytecode interface

--- bytecode.h
+++ bytecode.h
@@ -1,74 +1,58 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
-#include "array.h"
-#include "common.h"
-#include "table.h"
-#include "val.h"
+#include "table.h" // Table
 
-#define FOREACH_OPCODE(OPCODE)                                                 \
-  OPCODE(OP_ADD)                                                               \
-  OPCODE(OP_CONSTANT)                                                          \
-  OPCODE(OP_CONSTANT2)                                                         \
-  OPCODE(OP_DEF_GLOBAL)                                                        \
-  OPCODE(OP_DEF_GLOBAL2)                                                       \
-  OPCODE(OP_DIVIDE)                                                            \
-  OPCODE(OP_EQUAL)                                                             \
-  OPCODE(OP_FALSE)                                                             \
-  OPCODE(OP_GET_GLOBAL)                                                        \
-  OPCODE(OP_GET_GLOBAL2)                                                       \
-  OPCODE(OP_GET_LOCAL)                                                         \
-  OPCODE(OP_GET_LOCAL2)                                                        \
-  OPCODE(OP_GREATER)                                                           \
-  OPCODE(OP_JUMP)                                                              \
-  OPCODE(OP_JUMP_IF_FALSE)                                                     \
-  OPCODE(OP_JUMP_IF_TRUE)                                                      \
-  OPCODE(OP_LESS)                                                              \
-  OPCODE(OP_LOOP)                                                              \
-  OPCODE(OP_MULTIPLY)                                                          \
-  OPCODE(OP_NEGATE)                                                            \
-  OPCODE(OP_NIL)                                                               \
-  OPCODE(OP_NOT)                                                               \
-  OPCODE(OP_POP)                                                               \
-  OPCODE(OP_PRINT)                                                             \
-  OPCODE(OP_RETURN)                                                            \
-  OPCODE(OP_SET_GLOBAL)                                                        \
-  OPCODE(OP_SET_GLOBAL2)                                                       \
-  OPCODE(OP_SET_LOCAL)                                                         \
-  OPCODE(OP_SET_LOCAL2)                                                        \
-  OPCODE(OP_SUBTRACT)                                                          \
-  OPCODE(OP_TRUE)
+#include <stdint.h> // uint32_t, uint8_t
+#include <stdbool.h> // bool
 
-#define GENERATE_ENUM(ENUM) ENUM,
-typedef enum { FOREACH_OPCODE(GENERATE_ENUM) } OpCode;
+#define OPCODE(ENUM) ENUM,
+typedef enum {
+#include "opcodes.enum"
+} OpCode;
 #undef GENERATE_ENUM
 
 typedef struct {
-  size_t cap;
-  size_t len;
-  uint8_t *code;
-  // TODO: use a run-length encoding for lines
-  // until then, line[i] -> code[i]
-  size_t *lines;
-  Array consts;
-  Table const_pos;
+  struct {
+    uint32_t cap;
+    uint32_t len;
+    uint8_t *code;
+  } bytecode;
+  struct {
+    uint32_t cap;
+    uint32_t len;
+    uint32_t start_line;
+    uint32_t *lines;
+  } line_idx;
+  struct {
+    Table const_to_idx;
+    uint32_t idx;
+  };
+  Val *consts;
 } Chunk;
+
+typedef struct {
+  Chunk *chunk;
+  uint32_t cursor;
+} Bookmark;
 
 Chunk *chunk_init(Chunk *);
 void chunk_destroy(Chunk *);
 
-void chunk_write(Chunk *, uint8_t, size_t line);
-void chunk_write2(Chunk *, uint8_t[2], size_t line);
-void chunk_write3(Chunk *, uint8_t[3], size_t line);
-void chunk_emit_idx(Chunk *, OpCode, uint16_t idx, size_t line);
-bool chunk_emit_const(Chunk *, OpCode, Val *, size_t line);
+bool chunk_write(Chunk *, uint8_t, uint32_t line);
+bool chunk_writen(Chunk *, uint8_t const *, uint8_t n, uint32_t line);
 
-// chunk_seal() releases the memory held by const_pos table, and the consts,
-// code and lines arrays. After the chunk is sealed, newly added constants can
-// create duplicates.
-void chunk_seal(Chunk *);
+// chunk_write_op() writes op, op+1, op+2, op+3 based on the operand's size.
+bool chunk_write_op(Chunk *, uint8_t op, uint32_t operand, uint32_t line);
 
-size_t chunk_print_dis_instr(const Chunk *, size_t offset);
-void chunk_print_dis(const Chunk *);
+Bookmark chunk_bookmark(Chunk *);
+void bookmark_patch_left(Bookmark);
+void bookmark_patch_right(Bookmark);
+
+// chunk_finalize() creates the consts array and frees the const_idx table.
+void chunk_finalize(Chunk *);
+
+uint32_t chunk_print_dis_instr(Chunk const *, uint32_t offset);
+void chunk_print_dis(Chunk const *);
 
 #endif
